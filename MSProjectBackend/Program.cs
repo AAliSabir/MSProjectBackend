3f88caf019f81c0efb8d880e04aab963434ab1b0
Ahmed Ali Sabir <aa.sabir@outlook.com>
2022-06-02 14:48:32 +0500

    Dependencies injected

--- MSProjectBackend/Program.cs
+++ MSProjectBackend/Program.cs
@@ -13,14 +13,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //Repositories
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IVolunteerRepository, VolunteerRepository>();
 builder.Services.AddScoped<IRegistrationRepository, RegistrationRepository>();
+builder.Services.AddScoped<IAvailabilityRepository, AvailabilityRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICityRepository, CityRepository>();
+builder.Services.AddScoped<INGORepository, NGORepository>();
+builder.Services.AddScoped<INGOAppealRepository, NGOAppealRepository>();
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<IProvinceRepository, ProvinceRepository>();
+builder.Services.AddScoped<ISkillRepository, SkillRepository>();
 
 //Services
-builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<IVolunteerService, VolunteerService>();
 builder.Services.AddScoped<IRegistrationService, RegistrationService>();
+builder.Services.AddScoped<IAvailabilityService, AvailabilityService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICityService, CityService>();
+builder.Services.AddScoped<INGOService, NGOService>();
+builder.Services.AddScoped<INGOAppealService, NGOAppealService>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IProvinceService, ProvinceService>();
+builder.Services.AddScoped<ISkillService, SkillService>();
 builder.Services.AddCors();
 
 var app = builder.Build();
